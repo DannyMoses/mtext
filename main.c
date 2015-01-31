48190b882a56a0955a4d80592f08f0cba9741886
Dannymoses <dannymoses555@gmail.com>
2015-01-30 23:31:16 -0500

    Integrated the line counter into the main code.

--- main.c
+++ main.c
@@ -1,16 +1,34 @@
 /* fputs example */
 #include <stdio.h>
+#include <string.h>
 
 int main (int argc, char **argv)
 {
+	FILE * pFile;
 	if(argv[1] == NULL)
 	{
 		printf("Fatal Error: no file specified\n");
 		return 1;
 	}
+	else if(strcmp(argv[1], "-lines") == 0)
+	{
+		int c;
+ 		int n = 0;
+  		pFile=fopen (argv[2],"r");
+  		if (pFile==NULL) perror ("Error opening file");
+  		else
+  		{
+    		do {
+      				c = fgetc (pFile);
+      				if (c == '\n') n++;
+    				} while (c != EOF);
+    		fclose (pFile);
+    		printf ("The file contains %d lines.\n",n);
+  		}
+  		return 0;
+	}
 	else
 	{
-		FILE * pFile;
 		char sentence [256];
 
 		printf ("Enter sentence to append: ");
